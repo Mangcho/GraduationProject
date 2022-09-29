d7950fb9fd2c323d3bb1ada25ef4fd7574698f1f
Mangcho <bnb1324@naver.com>
2022-09-30 00:25:54 +0900

    수정 : /routes/api/auth 라우팅 구현 코드 수정 #3 조언에 따라 thenabel하고 callback 사용

--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -9,13 +9,13 @@ const router = express.Router();
 router.post('/api/login', wrapper(async (req, res) => {
     const userDto = { id: req.body.id, password: req.body.password };
     const status = auth.SignIn(userDto)
-        .then((a) => {
-            console.log("hello : ", a)
+        .then((result) => {
+            if (result) {
+                req.session.isAuth = true;
+            }
+            return res.json({ status: result })
         })
-    if (status) {
-        req.session.isAuth = true;
-    }
-    return res.json({ status: status })
+
 }))
 
 module.exports = router;

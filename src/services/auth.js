734da06c4f56bd7a63b598d3d79081386a0cb9f0
Mangcho <bnb1324@naver.com>
2022-09-30 15:10:55 +0900

    수정 : 로그인 서비스 구현 현재 로그인 결과 존재시 true 아닐경우 false 반환하게 설정 완료 애러 처리 고민중

--- src/services/auth.js
+++ src/services/auth.js
@@ -18,17 +18,20 @@ class AuthService {
      * @param {JSON} userDto - 사용자 계정 정보를 담고 있는 객체 id와 password를 가지고 있음
      */
     async SignIn(userDto) { // log-in
-        const hashedPW = GetHash(userDto.password);
-        const test = await Whitelist.create({ imei: "1234567890" })
-        const testFunc = await UserModel.create({ id: userDto.id, password: hashedPW, whitelist_imei: "1234567890" });
-        const isUserExist = await UserModel.findOne({
-            where: {
-                id: userDto.id,
-                password: hashedPW
-            }
-        })
-        console.log(isUserExist);
-
+        try {
+            const hashedPW = GetHash(userDto.password);
+            //const test = await Whitelist.create({ imei: "1234567890" })
+            //const testFunc = await UserModel.create({ id: userDto.id, password: hashedPW, whitelist_imei: "1234567890", name: "hal", age: 2 });
+            const isUserExist = await UserModel.findOne({
+                where: {
+                    id: userDto.id,
+                    password: hashedPW
+                }
+            })
+            return isUserExist === null ? true : false
+        } catch {
+            // Something Error catch
+        }
     }
 }
 

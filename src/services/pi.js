0de4327961190a74ad830ba78a19fc654c715ed6
Mangcho <bnb1324@naver.com>
2022-10-14 17:34:08 +0900

    수정 : service 전체 ESM 모듈화 service내 모든 model들 import 처리 및 export default를 사용하여 class export 처리 완료

--- src/services/pi.js
+++ src/services/pi.js
@@ -1,7 +1,6 @@
-const RasbpiModel = require("../models/rasbpi");
-
-class piDataService {
-  constructor() {}
+import  RasbpiModel from "../models/rasbpi.js";
+export default class piDataService {
+  constructor() { }
   /**
    * DB로 라즈베리파이의 센서 데이터를 저장하기 위해 사용
    * @param {Object} insertPiSensorDto - 라즈베리파이 센서 데이터값
@@ -13,14 +12,13 @@ class piDataService {
         timestamp: insertPiSensorDto.timestamp,
         raw: insertPiSensorDto.raw,
       });
-      console.log(typeof(isPiSensorAccepted))
+      console.log(typeof (isPiSensorAccepted))
       return isPiSensorAccepted === null ? false : true
-    } catch(error) {
+    } catch (error) {
       console.log(error)
     }
   }
 }
-module.exports = { piDataService };
 
 /*
 IMEI

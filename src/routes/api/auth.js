a95bc91d4d08254923f321aab8307ef2ac582459
Mangcho <bnb1324@naver.com>
2022-10-18 15:38:59 +0900

    추가 : 값 validation 추가 및 기능 추가 1. 기존에 존재하던 모든 auth 로직 empty 및 타입 검증 추가 2. 회원가입시 사용하는 imei값 검증 기능 추가

--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -1,21 +1,52 @@
 import express from "express";
+import { body, validationResult } from 'express-validator';
 import wrapper from "../../utils/wrapper.js"; // async wrapper
 import AuthService from "../../services/auth.js";
 
 const auth = new AuthService();
 const router = express.Router();
 
-router.post('/login', wrapper(async (req, res) => {
-    const compareUserDto = { id: req.body.id, password: req.body.password };
-    const state = await auth.SignIn(compareUserDto)
-    req.session.isAuth = state ? true : false
-    return res.json({ state: state })
-}))
+router.post('/login',
+    body('id').notEmpty().isEmail(),
+    body('password').notEmpty(),
+    wrapper(async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.json({ errors: errors.array() })
+        }
+        const compareUserDto = { id: req.body.id, password: req.body.password };
+        const state = await auth.SignIn(compareUserDto)
+        req.session.isAuth = state ? true : false
+        return res.json({ state })
+    }))
 
-router.post('/register', wrapper(async (req, res) => {
-    const createUserDto = { id: req.body.id, password: req.body.password, name: req.body.name, age: req.body.age, imei: req.body.imei };
-    const state = await auth.SignUp(createUserDto)
-    return res.json({ state: state })
-}))
+router.post('/register',
+    body('id').notEmpty().isEmail(),
+    body('password').notEmpty(),
+    body('name').notEmpty(),
+    body('age').notEmpty().isInt(),
+    body('imei').notEmpty().isAlphanumeric(),
+    wrapper(async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.json({ errors: errors.array() })
+        }
+        const createUserDto = { id: req.body.id, password: req.body.password, name: req.body.name, age: req.body.age, imei: req.body.imei };
+        const state = await auth.SignUp(createUserDto)
+        return res.json({ state })
+    }))
+
+router.post('/imei',
+    body('imei').notEmpty().isAlphanumeric(),
+    wrapper(async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.json({ errors: errors.array() })
+        }
+        const checkImeiDto = { imei: req.body.imei };
+        const state = await auth.CheckImei(checkImeiDto);
+        return res.json({ state })
+
+    }))
 
 export default router

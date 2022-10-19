7c8e7f70460a867246af931e642695864e1641e5
Mangcho <bnb1324@naver.com>
2022-10-19 21:23:02 +0900

    보완, 추가 : auth 서비스 내 기능 추가 및 보완 1.getHash util named import 2.계정생성 Dto 변수명 변경 3.로그인 테스트 코드 삭제 4.비밀번호 변경 서비스 추가

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,19 +1,19 @@
 import UserModel from "../models/user.js";
 import WhitelistModel from "../models/whitelist.js";
-import GetHash from "../utils/crypt.js"; // utils
+import { GetHash } from "../utils/crypt.js"; // utils
 
 export class AuthService {
 
     /**
     * 계정 생성을 처리하는 서비스
-    * @param {JSON} newUserDto - User account info
+    * @param {JSON} createUserDto - User account info
     */
-    async SignUp(newUserDto) {
+    async SignUp(createUserDto) {
         try {
-            const hashedPW = GetHash(newUserDto.password);
+            const hashedPW = GetHash(createUserDto.password);
             const newUser = await UserModel.create({
-                id: newUserDto.id, password: hashedPW, whitelist_imei: newUserDto.imei,
-                name: newUserDto.name, age: newUserDto.age
+                id: createUserDto.id, password: hashedPW, whitelist_imei: createUserDto.imei,
+                name: createUserDto.name, age: createUserDto.age
             });
             return newUser; // 생성이 불가능한 경우에는 error
         } catch (error) {
@@ -25,16 +25,31 @@ export class AuthService {
      * 로그인을 처리하는 서비스
      * @param {Object} compareUserDto - 사용자 계정 정보를 담고 있는 객체 id와 password를 가지고 있음
      */
-    async SignIn(compareUserDto) { // log-in
+    async SignIn(compareUserDto) {
         try {
             const hashedPW = GetHash(compareUserDto.password);
-            // test, testFunc는 로그인 테스트용 차후 삭제할 예정
-            const test = await WhitelistModel.create({ imei: "1234567890" })
-            const testFunc = await UserModel.create({ id: compareUserDto.id, password: hashedPW, whitelist_imei: "1234567890", name: "hal", age: 2 });
-
             const isUserExist = await UserModel.findOne({
                 where: {
                     id: compareUserDto.id,
+                    password: hashedPW
+                }
+            })
+            return isUserExist === null ? false : true // select 문에서 없을경우 null
+        } catch (error) {
+            console.log("SignIn Service", error);
+        }
+    }
+
+    /**
+     * 로그인을 처리하는 서비스
+     * @param {Object} logoutUserDto - 사용자 계정 정보를 담고 있는 객체 id와 password를 가지고 있음
+     */
+    async SignOut(logoutUserDto) {
+        try {
+            const hashedPW = GetHash(logoutUserDto.password);
+            const isUserExist = await UserModel.findOne({
+                where: {
+                    id: logoutUserDto.id,
                     password: hashedPW
                 }
             })
@@ -58,6 +73,24 @@ export class AuthService {
             return isImeiExist === null ? false : true
         } catch (error) {
             console.log("CheckImei Service", error);
+        }
+    }
+
+    /**
+     * 사용자에게 비밀번호를 입력받아 사용자 계정의 비밀번호를 변경하는 서비스
+     * @param {Object} changePasswordDto - 사용자가 입력한 비밀번호 값이 저장되어 있음
+     */
+    async ChangePassword(changePasswordDto) { // log-in
+        try {
+            const hashedPW = GetHash(changePasswordDto.password);
+            const changePassword = await UserModel.update({ password: hashedPW }, {
+                where: {
+                    id: changePasswordDto.id
+                }
+            })
+            return changePassword === null ? false : true
+        } catch (error) {
+            console.log("changePassword Service", error);
         }
     }
 }

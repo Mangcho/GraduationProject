7607a5df24b45d3c0ded7a573c2d17bb55dd283b
Mangcho <bnb1324@naver.com>
2022-10-19 14:53:10 +0900

    추가, 보완 : pi, auth 라우터 입력값 검증 및 named export로 보완 1. pi라우터는 입력값 전체 null인지 검증기능 추가 2. 파이 백엔드 알고리즘 개발에 따른 result 컬럼 입력 추가 3. auth라우터 전체 입력값 검증 추가 4. 모든 라우터 named export로 변경

--- src/routes/pi.js
+++ src/routes/pi.js
@@ -1,15 +1,32 @@
 import express from "express";
-import wrapper from "../utils/wrapper.js"; // async wrapper
-import piDataService from "../services/pi.js";
+import { wrapper } from "../utils/wrapper.js"; // async wrapper
+import { piDataService } from "../services/pi.js";
+import { body, validationResult } from "express-validator";
 
 const pi = new piDataService();
-
 const router = express.Router();
 
-router.post('', wrapper(async (req, res) => {
-    const insertPiSensorDto = { imei: req.body.imei, timestamp: req.body.timestamp, raw: req.body.raw }
-    const state = pi.SaveData(insertPiSensorDto);
-    return res.json({ state: state })
-}))
+router.post(
+  "",
+  body("imei").notEmpty().isAlphanumeric(),
+  body("raw").notEmpty(),
+  body("result").notEmpty(),
+  body("createdAt").notEmpty(),
+  wrapper(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({ errors: errors.array() });
+    }
+    const insertPiSensorDto = {
+      imei: req.body.imei,
+      raw: req.body.raw,
+      result: req.body.result,
+      createdAt: req.body.createdAt,
+    };
+    const state = await pi.SaveData(insertPiSensorDto);
+    //console.log(state);
+    return res.json({ state });
+  })
+);
 
-export default router
+export { router as piRouter };

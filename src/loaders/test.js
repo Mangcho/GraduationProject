86675933a19c2e733d5231041a7263b11b315c3e
Mangcho <bnb1324@naver.com>
2022-10-20 21:55:46 +0900

    수정 : user model의 변경에 따른 테스트 코드 컬럼명 변경

--- src/loaders/test.js
+++ src/loaders/test.js
@@ -2,12 +2,12 @@ import UserModel from "../models/user.js";
 import WhitelistModel from "../models/whitelist.js";
 
 export class Test {
-  constructor() {}
+  constructor() { }
   async syncData() {
     await WhitelistModel.create({ imei: "73ff34fce1" });
     // 비밀번호 = password
     await UserModel.create({
-      id: "bnb1324@naver.com",
+      email: "bnb1324@naver.com",
       password:
         "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8",
       whitelist_imei: "73ff34fce1",

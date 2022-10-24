73fc00be40ae3d639b60c05da83a90610795503b
Mangcho <bnb1324@naver.com>
2022-10-25 01:59:43 +0900

    단순화 : sequelize.js 컨벤션 변경

--- src/loaders/sequelize.js
+++ src/loaders/sequelize.js
@@ -3,7 +3,7 @@ export async function synchronize(db) {
 
     try {
         const response = await db.sequelize
-            .sync({ force: process.env.NODE_ENV === "development" ? true : false }) // DROP EVERY EXISTING TABLE when force = true
+            .sync({ force: process.env.NODE_ENV == "development" ? true : false }) // DROP EVERY EXISTING TABLE when force = true
         console.log("### DATABASE CONNECTED!!! ###");
     } catch (err) {
         console.error(err);

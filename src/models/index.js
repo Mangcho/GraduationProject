6b633c1b916cc1aa749c0048bf8da794a4420369
Mangcho <bnb1324@naver.com>
2022-10-25 01:56:54 +0900

    수정 : model index.js ENV 설정에 따라 세팅 변경하게 if문 사용

--- src/models/index.js
+++ src/models/index.js
@@ -3,12 +3,18 @@ const env = process.env;
 
 import { Sequelize, DataTypes } from 'sequelize';
 import {development, production } from "../settings/sequelize/config.js"
-const config = development;
+let config;
+if (env.NODE_ENV == 'development'){
+  config = development;
+} else {
+  config = production;
+}
 
 // Model Import
 import Whitelist from "./whitelist.js";
 import User from "./user.js";
 import Rasbpi from "./rasbpi.js";
+import Session from "./session.js";
 
 export const db = Sequelize;
 
@@ -24,11 +30,13 @@ db.sequelize = sequelize;
 db.whitelist = Whitelist;
 db.user = User;
 db.data = Rasbpi;
+db.session = Session;
 
 // Model Init
 Whitelist.init(sequelize);
 User.init(sequelize);
 Rasbpi.init(sequelize);
+Session.init(sequelize);
 
 // Model Association
 Whitelist.associate(db);

2e96603212bb5d1df8ba6e45c95e7976f8558af6
Mangcho <bnb1324@naver.com>
2022-10-26 00:03:42 +0900

    추가 : graph 라우터, 서비스 기능 일부 구현 1. liveState 라우터 서비스랑 연결 2. liveState 서비스 쿼리문 추가

--- src/routes/api/graph.js
+++ src/routes/api/graph.js
@@ -1,14 +1,26 @@
 import express from "express";
-import { body, validationResult } from "express-validator";
+import { header, body, validationResult } from "express-validator";
 import { wrapper } from "../../utils/wrapper.js"; // async wrapper
 import { GraphService } from "../../services/graph.js";
 
 const graph = new GraphService();
 const router = express.Router();
 
-router.post("/usage",
+router.post(
+    "/liveState",
+    body('imei').notEmpty(),
+    header('session.eid').notEmpty(),
+    header('session.imei').notEmpty(),
     wrapper(async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.json({ errors: errors.array() });
+        }
+        const getStatusDto = { imei: req.session.data.imei }
+        //id ..?
+        const state = await sendLiveStatus(getStatusDto);
 
-    }))
+    })
+)
 
 export { router as graphRouter };
--- src/services/graph.js
+++ src/services/graph.js
@@ -1,32 +1,52 @@
 import UserModel from "../models/user.js";
 import WhitelistModel from "../models/whitelist.js";
+import RasbpiModel from "../models/rasbpi.js";
 import { GetHash } from "../utils/crypt.js"; // utils
 
 export class GraphService {
+  /**
+   * 지정한 Imei를 가진 라즈베리파이의 센서 데이터를 확인하여 사용시간이 어떻게 되는지 반환하는 서비스
+   * 이때, 하루마다 사용한 시간을 분단위로 반환한다.
+   * @param {JSON} getStatusDto
+   */
+  async sendLiveStatus(getStatusDto) {
+    try {
+      const currentTime = new Date();
+      const pastTime = currentTime - 1000;
+      const getSecondSensorResult = await RasbpiModel.findAndCountAll({
+        attributes:['result'],
+        where: {
+          [Op.and]: [
+            { whitelist_imei: getStatusDto.imei },
+            { createdAt: { [Op.between]: [pastTime, currentTime] } },
+          ],
+        },
+        limit: 10,
+        raw: true,
+      });
+      //console.log(getSecondSensorResult.rows[0].result.backforth);
 
-    /**
-    * 지정한 Imei를 가진 라즈베리파이의 센서 데이터를 확인하여 사용시간이 어떻게 되는지 반환하는 서비스
-    * 이때, 하루마다 사용한 시간을 분단위로 반환한다.
-    * @param {JSON} sendStatusDto
-    */
-    async sendLiveStatus(sendStatusDto) {
-        try {
+      let sumBackforth = 0;
+      let sumleftRight = 0;
+      let sumAccuracy = 0;
 
-        } catch (error) {
-            console.log("sendLiveStatus Service", error);
-        }
+      //getSecondSensorResult.rows.forEach()
+
+      return getSecondSensorResult;
+    } catch (error) {
+      console.log("sendLiveStatus Service", error);
     }
+  }
 
-    /**
-    * 지정한 Imei를 가진 라즈베리파이의 센서 데이터를 확인하여 사용시간이 어떻게 되는지 반환하는 서비스
-    * 이때, 하루마다 사용한 시간을 분단위로 반환한다.
-    * @param {JSON} checkUsageDto
-    */
-    async checkUsage(checkUsageDto) {
-        try {
-
-        } catch (error) {
-            console.log("checkUsage Service", error);
-        }
+  /**
+   * 지정한 Imei를 가진 라즈베리파이의 센서 데이터를 확인하여 사용시간이 어떻게 되는지 반환하는 서비스
+   * 이때, 하루마다 사용한 시간을 분단위로 반환한다.
+   * @param {JSON} checkUsageDto
+   */
+  async checkUsage(checkUsageDto) {
+    try {
+    } catch (error) {
+      console.log("checkUsage Service", error);
     }
+  }
 }

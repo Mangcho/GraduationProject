4bd98512c2146b7a70eb21976a6349fd313e15b8
Mangcho <bnb1324@naver.com>
2022-10-26 00:05:32 +0900

    추가 : test.js 테스트 코드 추가 1. 어소시에이션이 없는 테이블들에 대한 join 쿼리 구현 2. 시간을 조건으로 가지고 limit을 가진 쿼리문 구현

--- src/loaders/test.js
+++ src/loaders/test.js
@@ -1,8 +1,11 @@
 import UserModel from "../models/user.js";
 import WhitelistModel from "../models/whitelist.js";
+import SessionModel from "../models/session.js";
+import RasbpiModel from "../models/rasbpi.js";
+import {Sequelize, Op} from "sequelize";
 
 export class Test {
-  constructor() { }
+  constructor() {}
   async syncData() {
     await WhitelistModel.create({ imei: "73ff34fce1" });
     // 비밀번호 = password
@@ -14,5 +17,48 @@ export class Test {
       name: "홍길동",
       age: 29,
     });
+  }
+
+  async joinQuery() {
+    const a = await SessionModel.findOne({
+      attributes:['data'],
+      where: 
+      {
+        [Op.and] : [
+          Sequelize.where(
+            Sequelize.fn("JSON_EXTRACT", 
+            Sequelize.col("data"), 
+            Sequelize.literal((`'$.eid'`)),
+            "bnb1324@naver.com"
+          )),
+          Sequelize.where(Sequelize.col("sid"),"1")
+        ]
+      }
+        ,    
+      // eid가 bnb1324@naver.com인 세션을 찾는다 -> 하나만
+      // SELECT `data` FROM `sessions` AS `sessions` WHERE (JSON_EXTRACT(`data`, '$.eid', 'bnb1324@naver.com') IS NULL AND `sid` = '1') LIMIT 1;
+      raw: true, // 이전 밸류 검색 사라짐
+    });
+    const b = JSON.parse(a.data);
+    console.log(b.imei);
+  }
+
+  async selectPi() {
+    const tta = new Date();
+    const ttb = tta - 1000;
+    console.log("aasadasd", tta.toISOString());
+    const abc = await RasbpiModel.findAndCountAll({
+      attributes:['result'],
+      where: {
+        [Op.and]:[
+          {whitelist_imei : "73ff34fce1"},
+          {createdAt:{[Op.between]:[ttb, tta]}}
+        ]
+      },
+      limit:10,
+      raw:true
+  })
+  console.log(abc.rows[0].result.backforth);
+  
   }
 }

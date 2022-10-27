1f489f40884677948d7ab9b15ef9d8eec459d54b
Mangcho <bnb1324@naver.com>
2022-10-28 02:02:19 +0900

    수정 : graph라우터 코드 수정 1. #9 코드리뷰 지적 반영하여 클래스 인스턴스 명칭 변경 graph -> graphService 2. Dto내 삽입 데이터 변경

--- src/routes/api/graph.js
+++ src/routes/api/graph.js
@@ -3,19 +3,19 @@ import { header, body, validationResult } from "express-validator";
 import { wrapper } from "../../utils/wrapper.js"; // async wrapper
 import { GraphService } from "../../services/graph.js";
 
-const graph = new GraphService();
+const graphService = new GraphService();
 const router = express.Router();
 
 router.post(
     "/liveState",
-    header('session.imei').notEmpty(),
+    header('session.data.imei').notEmpty(),
     wrapper(async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.json({ errors: errors.array() });
         }
-        const getStatusDto = { imei: req.session.data.imei }
-        const calDataPerSecond = await graph.sendLiveStatus(getStatusDto);
+        const getStatusDto = { imei: req.session.imei }
+        const calDataPerSecond = await graphService.sendLiveStatus(getStatusDto);
         return res.json(calDataPerSecond);
     })
 )

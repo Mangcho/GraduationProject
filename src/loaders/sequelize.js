5c92c8df140c64fa4942d2b28cbb165080c7c490
Mangcho <bnb1324@naver.com>
2022-10-31 09:03:03 +0900

    수정 : sequelize.js loader 코드 수정 프로젝트가 진행됨에 따라 force 옵션 대신 alter 옵션으로 데이터 보존

--- src/loaders/sequelize.js
+++ src/loaders/sequelize.js
@@ -3,7 +3,7 @@ export async function synchronize(db) {
 
     try {
         const response = await db.sequelize
-            .sync({ force: process.env.NODE_ENV == "development" ? true : false }) // DROP EVERY EXISTING TABLE when force = true
+            .sync({ alter: process.env.NODE_ENV == "development" ? true : false }) // DROP EVERY EXISTING TABLE when force = true / alter는 데이터는 그대로 냅둔다
         console.log("### DATABASE CONNECTED!!! ###");
     } catch (err) {
         console.error(err);

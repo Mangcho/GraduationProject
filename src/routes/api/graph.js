bf5455a67e211ad6d06f8aa587040c1c46587814
Mangcho <bnb1324@naver.com>
2022-10-31 09:04:44 +0900

    추가 : graph 라우터 척추신호등, 사용기록 기능 추가 1. 세션에서 로그인한 사용자의 imei 값을 수신하여 서버에 전송 2. 전송후 척추신호등, 사용기록 서비스에 전달 3. 원본 센서 데이터 기록은 아직 미완성

--- src/routes/api/graph.js
+++ src/routes/api/graph.js
@@ -8,15 +8,53 @@ const router = express.Router();
 
 router.post(
     "/liveState",
-    header('session.data.imei').notEmpty(),
     wrapper(async (req, res) => {
+        // session 감지 불가능
+        if(req.session.imei === null){
+            return res.json(null);
+        } 
+        //"73ff34fce1"req.session.imei
+        const getStatusDto = { imei: "73ff34fce1" }
+        const calDataPerSecond = await graphService.sendLiveStatus(getStatusDto);
+        return res.json(calDataPerSecond);
+    })
+)
+
+router.post(
+    "/usage",
+    //header('session.imei').notEmpty(), 
+    wrapper(async (req, res) => {
+        /*
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.json({ errors: errors.array() });
         }
-        const getStatusDto = { imei: req.session.imei }
-        const calDataPerSecond = await graphService.sendLiveStatus(getStatusDto);
-        return res.json(calDataPerSecond);
+        */
+        if(req.session.imei === null){
+            return res.json(null);
+        }
+        const getUsageDto = { imei: "73ff34fce1" }
+        const usagePerDay = await graphService.getUsage(getUsageDto);
+        return res.json(usagePerDay);
+    })
+)
+
+router.post(
+    "/original",
+    //header('session.imei').notEmpty(), 
+    wrapper(async (req, res) => {
+        /*
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.json({ errors: errors.array() });
+        }
+        */
+        if(req.session.imei === null){
+            return res.json(null);
+        }
+        const getOriginalDto = { imei: "73ff34fce1" }
+        const usagePerDay = await graphService.getUsage(getOriginalDto);
+        return res.json(usagePerDay);
     })
 )
 
